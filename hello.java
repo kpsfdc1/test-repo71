5acd8b5e15f316ffeaab75b53cd4c72d24f9386a
dev2 <dev2@mail.com>
2021-08-09 17:38:38 +0530

    from dev1

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-class hello
+class hello	#from dev1
 {
-	public static void main()
-	{
-		system.out.println("hello");
+	public static void main()	#from dev1
+	{	
+		system.out.println("hello from dev1");
 	}
 }
